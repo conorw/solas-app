1437213238228b6f273c33aa3e510303488c9a65
Conor Woods <conorw@gmail.com>
2023-02-03 12:50:13 +0000

    add stats link

--- src/routes/admin/stats/+page.svelte
+++ src/routes/admin/stats/+page.svelte
@@ -15,7 +15,7 @@
 	export let data: PageData;
 	let active = 'All';
 	let stats = data.stats;
-	function groupBy(list, keyGetter, sort=true) {
+	function groupBy(list, keyGetter, sort = true) {
 		const map = new Map();
 		list.forEach((item) => {
 			const key = keyGetter(item);
@@ -29,7 +29,10 @@
 		return sort ? [...map.entries()].sort((a, b) => b[1].length - a[1].length) : [...map.entries()];
 	}
 	let groupedService = groupBy(data.stats, (stat: { ServiceName: any }) => stat.ServiceName);
-	let groupedByMonth = groupBy(data.stats, (stat: attendance) => DateTime.fromISO(stat.Date!).monthLong);
+	let groupedByMonth = groupBy(
+		data.stats,
+		(stat: attendance) => DateTime.fromISO(stat.Date!).monthLong
+	);
 	let groupedUser = groupBy(data.stats, (stat: attendance) => stat['Person Id']);
 
 	console.log(groupedUser);
@@ -43,7 +46,10 @@
 			console.log(e);
 			data.fromDate = DateTime.fromJSDate(e).toFormat('yyyy-MM-dd');
 			$page.url.searchParams.set('fromDate', data.fromDate);
-			await goto($page.url.pathname + '?' + $page.url.searchParams.toString(), { replaceState: true, invalidateAll: true });
+			await goto($page.url.pathname + '?' + $page.url.searchParams.toString(), {
+				replaceState: true,
+				invalidateAll: true
+			});
 		}
 	}}
 	selected={new Date(data.fromDate)}
@@ -60,81 +66,75 @@
 	}}
 	selected={new Date(data.toDate)}
 />
-<TabBar tabs={['All', 'By Service', 'By Person']} let:tab bind:active>
-	<!-- Note: the `tab` property is required! -->
-	<Tab {tab}>
-		<Label>{tab}</Label>
-	</Tab>
-</TabBar>
+
 {#if stats}
-	{#if active === 'All'}
-		<!-- {#each stats as stat}
+	<!-- {#each stats as stat}
 			<div class="stat">
 				<h2>{stat.Date}</h2>
 				<p>{stat['Person Name']}</p>
 			</div>
 		{/each} -->
-		<h2>Total Unique People: {groupedUser.length}</h2>
-		<h2>Total Sessions: {data.stats.length}</h2>
-		<h2>Most Popular Service: {popularService}</h2>
-		<BarChartSimple
-			data={groupedService.map((t) => {
-				return { group: t[0], value: t[1].length };
-			})}
-			options={{
-				title: 'Totals',
-				height: '400px',
-				axes: {
-					left: { mapsTo: 'value' },
-					bottom: { mapsTo: 'group', scaleType: 'labels' }
-				}
-			}}
-		/>
-		<DataTable table$aria-label="Service list">
-			<Head>
+	<h2>Total Unique People: {groupedUser.length}</h2>
+	<h2>Total Sessions: {data.stats.length}</h2>
+	<h2>Most Popular Service: {popularService}</h2>
+	<BarChartSimple
+		data={groupedService.map((t) => {
+			return { group: t[0], value: t[1].length };
+		})}
+		options={{
+			title: 'Totals',
+			height: '400px',
+			axes: {
+				left: { mapsTo: 'value' },
+				bottom: { mapsTo: 'group', scaleType: 'labels' }
+			}
+		}}
+	/>
+	<DataTable table$aria-label="Service list">
+		<Head>
+			<Row>
+				<Cell columnId="name">
+					<Label>Name</Label>
+				</Cell>
+				<Cell columnId="total">
+					<Label>Sessions</Label>
+				</Cell>
+			</Row>
+		</Head>
+		<Body>
+			{#each groupedService as item}
 				<Row>
-					<Cell columnId="name">
-						<Label>Name</Label>
-					</Cell>
-					<Cell columnId="total">
-						<Label>Sessions</Label>
+					<Cell>{item?.[0]}</Cell>
+					<Cell>
+						{item?.[1].length}
 					</Cell>
 				</Row>
-			</Head>
-			<Body>
-				{#each groupedService as item}
-					<Row>
-						<Cell>{item?.[0]}</Cell>
-						<Cell>
-							{item?.[1].length}
-						</Cell>
-					</Row>
-				{/each}
-			</Body>
-		</DataTable>
-		<DataTable table$aria-label="User list">
-			<Head>
+			{/each}
+		</Body>
+	</DataTable>
+	<DataTable table$aria-label="User list">
+		<Head>
+			<Row>
+				<Cell columnId="name">
+					<Label>Name</Label>
+				</Cell>
+				<Cell columnId="total">
+					<Label>Sessions</Label>
+				</Cell>
+			</Row>
+		</Head>
+		<Body>
+			{#each groupedUser as item}
 				<Row>
-					<Cell columnId="name">
-						<Label>Name</Label>
-					</Cell>
-					<Cell columnId="total">
-						<Label>Sessions</Label>
+					<Cell><a target="_blank" href={`/people/${item?.[1][0]['Person Id']}`}>{item?.[1][0]['Person Name']}</a></Cell>
+					<Cell>
+						{item?.[1].length}
 					</Cell>
 				</Row>
-			</Head>
-			<Body>
-				{#each groupedUser as item}
-					<Row>
-						<Cell>{item?.[1][0]['Person Name']}</Cell>
-						<Cell>
-							{item?.[1].length}
-						</Cell>
-					</Row>
-				{/each}
-			</Body>
-		</DataTable>
-		<BarChartSimple
+			{/each}
+		</Body>
+	</DataTable>
+	<BarChartSimple
 		data={groupedByMonth.map((t) => {
 			return { group: t[0], value: t[1].length };
 		})}
@@ -147,5 +147,4 @@
 			}
 		}}
 	/>
-	{/if}
 {/if}
