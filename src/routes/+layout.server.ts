78bf9e31f662fff35b905cf5e8639cff21aa636f
Conor Woods <conorw@gmail.com>
2023-02-03 11:58:37 +0000

    dont redirect reset password

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -5,7 +5,7 @@ import { supabaseClient } from '$lib/supabase'
 export const load: LayoutServerLoad = async (event) => {
 
 	const session = await getServerSession(event)
-	if(!session && !event.url.pathname.includes('login')) throw redirect(303, "/login")
+	if(!session && (!event.url.pathname.includes('login') && !event.url.pathname.includes('reset-password'))) throw redirect(303, "/login")
 	if(event.url.pathname === '/') throw redirect(302, "/attendance")
 	const profile = await (await supabaseClient.from('profiles').select('*').eq('id', session?.user?.id).single()).data;
 	if(profile && !profile?.isAdmin && !event.url.pathname.includes('admin')) throw redirect(303, "/attendance")
