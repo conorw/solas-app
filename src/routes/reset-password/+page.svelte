57201f0eaaa90c1a282a80ac0a005304ef7a85de
Conor Woods <conorw@gmail.com>
2023-02-08 23:59:32 +0000

    show reset password field

--- src/routes/reset-password/+page.svelte
+++ src/routes/reset-password/+page.svelte
@@ -5,7 +5,7 @@
 	<h1>Reset Password</h1>
 	<form action="?/reset" method="POST" class="auth-form">
 		<label for=""> New Password </label>
-		<input type="password" name="password" />
+		<input name="password" />
 		<button type="submit" class="btn btn-primary">Reset</button>
 	</form>
 </main>
