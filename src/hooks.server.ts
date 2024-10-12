06b42fa7b7e1c5488c061af1f88d189de322c0f3
Conor Woods <conorw@gmail.com>
2024-10-12 21:53:53 +0100

    update supabase auth

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,37 +1,55 @@
-import {
-	PUBLIC_SUPABASE_URL,
-	PUBLIC_SUPABASE_ANON_KEY
-} from '$env/static/public';
-import { createSupabaseServerClient } from '@supabase/auth-helpers-sveltekit';
-import type { Handle } from '@sveltejs/kit';
+import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public'
+import { createServerClient } from '@supabase/ssr'
+import type { Handle } from '@sveltejs/kit'
 
 export const handle: Handle = async ({ event, resolve }) => {
-	event.locals.supabase = createSupabaseServerClient({
-		supabaseUrl: PUBLIC_SUPABASE_URL,
-		supabaseKey: PUBLIC_SUPABASE_ANON_KEY,
-		event
-	});
+  event.locals.supabase = createServerClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY, {
+    cookies: {
+      getAll() {
+        return event.cookies.getAll()
+      },
+      setAll(cookiesToSet) {
+        /**
+         * Note: You have to add the `path` variable to the
+         * set and remove method due to sveltekit's cookie API
+         * requiring this to be set, setting the path to an empty string
+         * will replicate previous/standard behavior (https://kit.svelte.dev/docs/types#public-types-cookies)
+         */
+        cookiesToSet.forEach(({ name, value, options }) =>
+          event.cookies.set(name, value, { ...options, path: '/' })
+        )
+      },
+    },
+  })
 
-	/**
-	 * a little helper that is written for convenience so that instead
-	 * of calling `const { data: { session } } = await supabase.auth.getSession()`
-	 * you just call this `await getSession()`
-	 */
-	event.locals.getSession = async () => {
-		const {
-			data: { session }
-		} = await event.locals.supabase.auth.getSession();
-		return session;
-	};
+  /**
+   * Unlike `supabase.auth.getSession()`, which returns the session _without_
+   * validating the JWT, this function also calls `getUser()` to validate the
+   * JWT before returning the session.
+   */
+  event.locals.safeGetSession = async () => {
+    const {
+      data: { session },
+    } = await event.locals.supabase.auth.getSession()
+    if (!session) {
+      return { session: null, user: null }
+    }
 
-	return resolve(event, {
-		/**
-		 * There´s an issue with `filterSerializedResponseHeaders` not working when using `sequence`
-		 *
-		 * https://github.com/sveltejs/kit/issues/8061
-		 */
-		filterSerializedResponseHeaders(name) {
-			return name === 'content-range';
-		}
-	});
-};
+    const {
+      data: { user },
+      error,
+    } = await event.locals.supabase.auth.getUser()
+    if (error) {
+      // JWT validation has failed
+      return { session: null, user: null }
+    }
+
+    return { session, user }
+  }
+
+  return resolve(event, {
+    filterSerializedResponseHeaders(name) {
+      return name === 'content-range' || name === 'x-supabase-api-version'
+    },
+  })
+}
--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,17 +1,18 @@
 import type { LayoutServerLoad } from "./$types"
 import { redirect } from "@sveltejs/kit"
 
-export const load: LayoutServerLoad = async ({ locals, url }) => {
-
-	const session = await locals.getSession()
+export const load: LayoutServerLoad = async ({ locals: { safeGetSession, supabase }, cookies, url }) => {
+	const { session, user } = await safeGetSession()
+  
 	if (!session?.user && (!url.pathname.includes('login') && !url.pathname.includes('reset-password'))) throw redirect(303, "/login")
 	if (url.pathname === '/') throw redirect(302, "/attendance")
-	const profile = await (await locals.supabase.from('profiles').select('*').eq('id', session?.user?.id).single()).data;
+	const profile = await (await supabase.from('profiles').select('*').eq('id', session?.user?.id).single()).data;
 
 	if (url.pathname.includes('admin') && (!profile || !profile?.isAdmin)) throw redirect(303, "/attendance")
 
 	return {
 		session,
 		profile,
+		cookies: cookies.getAll()
 	}
 }
