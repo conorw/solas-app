79b23f0a10ca24db7a67772a2c76048a6320be5a
Conor Woods <conorw@gmail.com>
2025-01-23 23:50:37 +0000

    fix script preprocess

--- svelte.config.js
+++ svelte.config.js
@@ -7,10 +7,7 @@ const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	preprocess: [
-		vitePreprocess(),
-		preprocess({
-			postcss: true
-		})
+		vitePreprocess({script: true})
 	],
 
 	kit: {
