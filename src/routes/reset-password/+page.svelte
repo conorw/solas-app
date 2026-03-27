b161e4eccca656d3fcbe7f02f14577ea4ae895c2
Conor Woods <conor.woods@plotbox.io>
2026-03-27 20:41:11 +0000

    Fix input type for password field in reset password form

--- src/routes/reset-password/+page.svelte
+++ src/routes/reset-password/+page.svelte
@@ -5,7 +5,7 @@
 	<h1>Reset Password</h1>
 	<form action="?/reset" method="POST" class="auth-form">
 		<label for=""> New Password </label>
-		<input name="password" />
+		<input type="password" name="password" />
 		<button type="submit" class="btn btn-primary">Reset</button>
 	</form>
 </main>
